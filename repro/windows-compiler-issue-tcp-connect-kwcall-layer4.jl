a265ece631f555ddf86bd9ae721e3cbaeb61977a
Jacob Quinn <quinn.jacobd@gmail.com>
2026-03-05 18:33:13 -0700

    Add layer-four TCP compiler repro

---
+++ repro/windows-compiler-issue-tcp-connect-kwcall-layer4.jl
@@ -0,0 +1,4 @@
+using Reseau
+const NC = Reseau.TCP
+const KW = NamedTuple{(:local_addr, :connect_deadline_ns, :cancel_state), Tuple{Nothing, Int64, Nothing}}
+Base.return_types(Core.kwcall, Tuple{KW, typeof(NC.connect_tcp_fd!), NC.SocketAddrV4})
