5f811ce71eb793b98256a4ba28e6b9767eedf3e2
Jacob Quinn <quinn.jacobd@gmail.com>
2026-04-17 00:13:18 -0600

    stabilize windows tcp deadline accept test

--- test/tcp_tests.jl
+++ test/tcp_tests.jl
@@ -354,10 +354,18 @@ end
                 @test_throws NC.DeadlineExceededError NC.accept(listener)
 
                 NC.set_deadline!(listener, Int64(0))
-                accept_task = errormonitor(@async NC.accept(listener))
+                accept_task = errormonitor(Threads.@spawn begin
+                    try
+                        return NC.accept(listener)
+                    catch err
+                        return err
+                    end
+                end)
                 client = NC.connect(NC.loopback_addr(Int(laddr.port)))
                 @test _nc_wait_task_done(accept_task, 2.0) != :timed_out
-                server = fetch(accept_task)
+                server_result = fetch(accept_task)
+                server_result isa Exception && throw(server_result)
+                server = server_result
                 @test server isa NC.Conn
 
                 @test close(listener) === nothing
