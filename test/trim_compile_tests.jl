e511f0c304756c0e101a0dd1fb3e32fc04faff47
Alex Arslan <ararslan@comcast.net>
2026-06-03 12:30:58 -0700

    Add an option to skip trim compilation tests, skip on FreeBSD CI
    Fixes need to land in PackageCompiler and JuliaC before it will work.

--- test/trim_compile_tests.jl
+++ test/trim_compile_tests.jl
@@ -167,6 +167,9 @@ function _parse_trim_verify_totals(output::String)
 end
 
 @testset "Trim compile" begin
+    if !Base.get_bool_env("RESEAU_RUN_TRIM_TESTS", true)
+        println("[trim] skip RESEAU_RUN_TRIM_TESTS=false: user requested to skip trim compilation tests")
+        @test true
     if !_TRIM_SUPPORTED
         println("[trim] skip Julia < 1.12: JuliaC trim compilation is unavailable")
         @test true
